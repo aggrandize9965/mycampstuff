cfe6c0f3d2e2fd0b2b4ed10b1f4a089824925e38
aggrandize9965 <52671061+aggrandize9965@users.noreply.github.com>
2019-07-18 14:50:49 -0600

    Add files via upload

---
+++ rps.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+"""
+File: rps.py
+Name:
+
+A rock-paper-scissors game against the CPU
+Concepts covered: Random, IO, if/else, printing
+"""
+
+import random
+import sys
+
+def main():
+    # Code here
+    rps = input("choose rock,paper or scissors (r/p/s)")
+    choices = ["r","p",'s']
+    
+    for answer in choices:
+        if rps = 
+    
+    
+    comp = "random.choice(choices)"
+    
+            
+        
+    
+
+
+def ai_guess():
+    # Code here
+    pass
+
+
+def checkWin(p1, p2):
+    # Code here
+    pass
+
+
+if __name__ == "__main__":
+    main()
