1b752fd3741e9af3a21bf11bd0e3a4597f4f5304
aggrandize9965 <52671061+aggrandize9965@users.noreply.github.com>
2019-07-19 15:08:42 -0600

    Add files via upload

--- dice.py
+++ dice.py
@@ -12,11 +12,14 @@ import random
 
 def main():
     # Code here
+    print("this is a dice simulator.")
+    
     answer = "c"
     while (answer == "c"):
-        answer = input("Type c to roll the dice")
+        answer = input("Type c to roll the dice ")
         randum = random.randint(1,6)
         print("you rolled a " +str(randum)+"!")
+        print("would you like to go again?")
       
     
 
--- rps.py
+++ rps.py
@@ -13,28 +13,14 @@ import sys
 
 def main():
     # Code here
-    rps = input("choose rock,paper or scissors (r/p/s)")
-    choices = ["r","p",'s']
-    
-    for answer in choices:
-        if rps = 
-    
-    
-    comp = "random.choice(choices)"
-    
-            
-        
-    
 
 
 def ai_guess():
     # Code here
-    pass
 
 
 def checkWin(p1, p2):
     # Code here
-    pass
 
 
 if __name__ == "__main__":
